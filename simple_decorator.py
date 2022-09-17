2dc0f1fdb193f2f55e62e162c89ea9463bff3277
yactouat <yactouat@hotmail.com>
2022-09-17 16:47:47 +0200

    initial commit

---
+++ simple_decorator.py
@@ -0,0 +1,20 @@
+def simple_decorator(decorated_function):
+    # this illustrates how decorators wrap a function to add a functionality
+    def wrapper():
+        print("printed BEFORE decorated_function is called")
+        decorated_function()
+        print("printed AFTER decorated_function is called")
+    return wrapper
+
+
+def some_func():
+    print("doing stuff")
+
+
+# this is how `some_func`
+# becomes actually decorated
+some_func = simple_decorator(some_func)
+
+
+# calling the function
+some_func()

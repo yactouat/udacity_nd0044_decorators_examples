57290f816d1c261f1bf018d36c2d7fc12d5534d9
yactouat <yactouat@hotmail.com>
2022-09-17 17:39:32 +0200

    prettier decorators

---
+++ prettier_decorator.py
@@ -0,0 +1,17 @@
+def prettier_decorator(decorated_function):
+    # this illustrates how decorators wrap a function to add a functionality
+    def wrapper():
+        print("printed BEFORE decorated_function is called")
+        decorated_function()
+        print("printed AFTER decorated_function is called")
+    return wrapper
+
+
+# this way of wrapping a function does the same thing than =>
+# some_func = prettier_decorator(some_func)
+@prettier_decorator
+def some_func():
+    print("doing stuff")
+
+
+some_func()

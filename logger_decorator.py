04fd4ebd1a87a93d0f6f2d2c39dddf4461bed366
yactouat <yactouat@hotmail.com>
2022-09-18 15:51:41 +0200

    adding logger decorator

---
+++ logger_decorator.py
@@ -0,0 +1,23 @@
+from functools import wraps
+import logging
+
+import custom_logger
+
+
+def logger(func):
+    '''decorator to log function calls'''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger = logging.getLogger("custom_logger")
+        logger.debug(f"calling {func.__name__!r}")
+        value = func(*args, **kwargs)
+        return value
+    return wrapper
+
+
+@logger
+def some_func():
+    print("I do something")
+
+
+some_func()
